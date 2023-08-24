191bcd3a3824f90a36357141f7045fda24515a1a
okkhoy <okkhoy@gmail.com>
2023-08-25 00:36:04 +0800

    Add code to track interest rates and returned loans

--- src/main/java/Main.java
+++ src/main/java/Main.java
@@ -1,24 +1,38 @@
 public class Main {
-
     static int[] amounts = new int[100];
     static String[] borrowers = new String[100];
+    static boolean[] isPaid = new boolean[100];
+    static double[] interestRates = new double[100];
     static int loansCount = 0;
 
-    public static void addLoan(String borrower, int amount) {
+    public static void addLoan(String borrower, int amount, double interestRate) {
         borrowers[loansCount] = borrower;
         amounts[loansCount] = amount;
+        interestRates[loansCount] = interestRate;
         loansCount++;
+    }
+
+    public static void setAsPaid(String borrower) {
+        for (int i = 0; i < loansCount; i++) {
+            if (borrowers[i].equals(borrower)) {
+                isPaid[i] = true;
+            }
+        }
     }
 
     public static void listLoans() {
         for (int i = 0; i < loansCount; i++) {
-            System.out.println(borrowers[i] + ": " + amounts[i]);
+            if (!isPaid[i]) {
+                System.out.println(borrowers[i] + ": " + amounts[i]);
+            }
         }
     }
 
     public static void main(String[] args) {
-        addLoan("Alice Betsy", 10);
-        addLoan("Ben Chee", 20);
+        addLoan("Alice Betsy", 10, 0.1);
+        addLoan("Ben Chee", 20, 0.2);
+        addLoan("Chris Davis", 30, 0.3);
+        setAsPaid("Ben Chee");
         listLoans();
     }
 }
