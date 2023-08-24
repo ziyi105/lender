6a6fa7e2f9712187ceda53ddc824a365f62aad1e
okkhoy <okkhoy@gmail.com>
2023-08-24 23:43:29 +0800

    Add mechanism to add loans and list the loans

--- src/main/java/Main.java
+++ src/main/java/Main.java
@@ -1,6 +1,20 @@
 public class Main {
 
+    static int[] loans = new int[100];
+    static int loansCount = 0;
+
+    public static void addLoan(int amount) {
+        loans[loansCount] = amount;
+        loansCount++;
+    }
+
+    public static void listLoans() {
+        for (int i = 0; i < loansCount; i++) {
+            System.out.println(loans[i]);
+        }
+    }
+
     public static void main(String[] args) {
-        
+
     }
 }
