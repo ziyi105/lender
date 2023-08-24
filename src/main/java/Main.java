6508452994acbffaae3fa655d99e9c0ecbade64a
okkhoy <okkhoy@gmail.com>
2023-08-25 00:37:14 +0800

    Add method to calculate profit

--- src/main/java/Main.java
+++ src/main/java/Main.java
@@ -28,11 +28,23 @@ public class Main {
         }
     }
 
+    public static double calculateProfitEarned() {
+        double profit = 0;
+        for (int i = 0; i < loansCount; i++) {
+            // if the loan was repaid, add the interest to profit
+            if (isPaid[i]) {
+                profit = profit + (amounts[i] * interestRates[i]);
+            }
+        }
+        return profit;
+    }
+
     public static void main(String[] args) {
         addLoan("Alice Betsy", 10, 0.1);
         addLoan("Ben Chee", 20, 0.2);
         addLoan("Chris Davis", 30, 0.3);
         setAsPaid("Ben Chee");
         listLoans();
+        System.out.println("profit: " + calculateProfitEarned());
     }
 }
